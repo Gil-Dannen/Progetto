49440fd162d24a74a225b7dd79a86546e6458595
Mattia Sacchi <mattia.sacchi@automacengineering.it>
2023-10-02 15:58:20 +0200

    Pulizia intermedia ble_manager

--- src/ble_manager.c
+++ src/ble_manager.c
@@ -2,6 +2,7 @@
 #include "ble_interface.h"
 #include "stm32l4xx_hal_spi.h"
 #include "io_manager.h"
+#include "time_manager.h"
 
 // BLE Commands
 
@@ -44,9 +45,21 @@ uint8_t UPDATE_CHAR[] = {0x01, 0x06, 0xFD, 0x09, 0xff, 0xff, 0xff, 0xff, 0x01, 0
 
 uint8_t EVENT_DISCONNECTED[] = {0x04, 0x05, 0x04, 0x00};
 
-#define BLE_OK 0
 
-#define EVENT_STARTUP_SIZE 6
+static uint8_t fetchMasterHeader[] = {0x0b, 0x00, 0x00, 0x00, 0x00};
+
+static uint8_t sendMasterHeader[] = {0x0a, 0x00, 0x00, 0x00, 0x00};
+
+typedef enum
+{
+    BE_OK = 0,
+    BE_ERROR_NO_DATA,
+    BE_ERROR,
+    BE_ERROR2,
+
+}BleEvent;
+
+
 
 // 1 atribute service +2 attribute char readable+3*(2 NOTIFYABLE READABLE charachteristics)
 
@@ -64,41 +77,40 @@ uint8_t deviceName[] = {'S', 'T', 'M', '3', '2', 'L', '4', '7', '5'};
 
 uint8_t buffer[255];
 
-uint16_t stackInitCompleteFlag = 0;
+
 uint8_t *rxEvent;
+static uint16_t stackInitCompleteFlag = 0;
 
 void ble_init()
 {
     resetBleModule();
 
+    static const size_t eventStartupSize = 7;
+
     // fetching the reset event
-    rxEvent = (uint8_t *)malloc(EVENT_STARTUP_SIZE);
-    int res;
+    rxEvent = (uint8_t *)malloc(eventStartupSize);
+    int res = fetchBleEvent(rxEvent, eventStartupSize);
 
-    res = fetchBleEvent(rxEvent, EVENT_STARTUP_SIZE);
-
-    if (res == BLE_OK)
+    if (res == BE_OK)
     {
-        res = checkEventResp(rxEvent, EVENT_STATUP_DATA, EVENT_STARTUP_SIZE);
-        if (res == BLE_OK)
-        {
+        res = checkEventResp(rxEvent, EVENT_STATUP_DATA, eventStartupSize);
+        if (res == BE_OK)
             stackInitCompleteFlag |= 0x01;
-        }
+        
     }
+
     sleep(10);
     free(rxEvent);
 
     // INIT GATT
 
-    if (BLE_command(ACI_GATT_INIT, sizeof(ACI_GATT_INIT), ACI_GATT_INIT_COMPLETE, sizeof(ACI_GATT_INIT_COMPLETE), 0) == BLE_OK)
-    {
+    if (BLE_command(ACI_GATT_INIT, sizeof(ACI_GATT_INIT), ACI_GATT_INIT_COMPLETE, sizeof(ACI_GATT_INIT_COMPLETE), 0) == BE_OK)
         stackInitCompleteFlag |= 0x02;
-    }
     free(rxEvent);
 
     // INIT GAP, actually the handle that i get is a GATT handle of a service, will change the name later
 
-    if (BLE_command(ACI_GAP_INIT, sizeof(ACI_GAP_INIT), ACI_GAP_INIT_COMPLETE, sizeof(ACI_GAP_INIT_COMPLETE), 3) == BLE_OK)
+    if (BLE_command(ACI_GAP_INIT, sizeof(ACI_GAP_INIT), ACI_GAP_INIT_COMPLETE, sizeof(ACI_GAP_INIT_COMPLETE), 3) == BE_OK)
     {
         stackInitCompleteFlag |= 0x04;
         memcpy(GAP_SERVICE_HANDLE, rxEvent + 7, 2);
@@ -114,26 +126,20 @@ void ble_init()
 
     // INIT AUTH
 
-    if (BLE_command(ACI_GAP_SET_AUTH, sizeof(ACI_GAP_SET_AUTH), ACI_GAP_SET_AUTH_RESP, sizeof(ACI_GAP_SET_AUTH_RESP), 0) == BLE_OK)
-    {
+    if (BLE_command(ACI_GAP_SET_AUTH, sizeof(ACI_GAP_SET_AUTH), ACI_GAP_SET_AUTH_RESP, sizeof(ACI_GAP_SET_AUTH_RESP), 0) == BE_OK)
         stackInitCompleteFlag |= 0x10;
-    }
     free(rxEvent);
 
     // SET_TX_LEVEL
 
-    if (BLE_command(ACI_HAL_SET_TX_POWER_LEVEL, sizeof(ACI_HAL_SET_TX_POWER_LEVEL), ACI_HAL_SET_TX_POWER_LEVEL_COMPLETE, sizeof(ACI_HAL_SET_TX_POWER_LEVEL_COMPLETE), 0) == BLE_OK)
-    {
+    if (BLE_command(ACI_HAL_SET_TX_POWER_LEVEL, sizeof(ACI_HAL_SET_TX_POWER_LEVEL), ACI_HAL_SET_TX_POWER_LEVEL_COMPLETE, sizeof(ACI_HAL_SET_TX_POWER_LEVEL_COMPLETE), 0) == BE_OK)
         stackInitCompleteFlag |= 0x20;
-    }
     free(rxEvent);
 
     // SET SCAN RESPONSE DATA
 
-    if (BLE_command(HCI_LE_SET_SCAN_RESPONSE_DATA, sizeof(HCI_LE_SET_SCAN_RESPONSE_DATA), HCI_LE_SET_SCAN_RESPONSE_DATA_COMPLETE, sizeof(HCI_LE_SET_SCAN_RESPONSE_DATA_COMPLETE), 0) == BLE_OK)
-    {
+    if (BLE_command(HCI_LE_SET_SCAN_RESPONSE_DATA, sizeof(HCI_LE_SET_SCAN_RESPONSE_DATA), HCI_LE_SET_SCAN_RESPONSE_DATA_COMPLETE, sizeof(HCI_LE_SET_SCAN_RESPONSE_DATA_COMPLETE), 0) == BE_OK)
         stackInitCompleteFlag |= 0x40;
-    }
     free(rxEvent);
 
     // This will start the advertisment,
@@ -142,86 +148,70 @@ void ble_init()
     sleep(10);
 }
 
+
 int fetchBleEvent(uint8_t *container, int size)
 {
 
-    uint8_t master_header[] = {0x0b, 0x00, 0x00, 0x00, 0x00};
+    uint8_t * master_header = fetchMasterHeader;
     uint8_t slave_header[5];
 
     // Wait until it is available an event coming from the BLE module (GPIO PIN COULD CHANGE ACCORDING TO THE BOARD)
-    if (readDigital(MF_BleInt))
+    if (!readDigital(MF_BleInt))
+        return BE_ERROR;
+
+    sleep(5);
+    // PIN_CS of SPI2 LOW
+    setDigital(MF_BleCS, GPIO_PIN_RESET);
+
+    // SPI2 in this case, it could change according to the board
+    // we send a byte containing a request of reading followed by 4 dummy bytes
+    HAL_SPI_TransmitReceive(&hspi3, master_header, slave_header, 5, 1);
+    setDigital(MF_BleCS, GPIO_PIN_SET);
+    sleep(1);
+    setDigital(MF_BleCS, GPIO_PIN_RESET);
+
+    HAL_SPI_TransmitReceive(&hspi3, master_header, slave_header, 5, 1);
+
+    // let's get the size of data available
+    int dataSize = (slave_header[3] | slave_header[4] << 8);
+
+    if (dataSize > size)
+        dataSize = size;
+    
+    if (dataSize > 0)
     {
-
-        sleep(5);
-        // PIN_CS of SPI2 LOW
-        setDigital(MF_BleCS, GPIO_PIN_RESET);
-
-        // SPI2 in this case, it could change according to the board
-        // we send a byte containing a request of reading followed by 4 dummy bytes
-        HAL_SPI_TransmitReceive(&hspi3, master_header, slave_header, 5, 1);
-        setDigital(MF_BleCS, GPIO_PIN_SET);
-        sleep(1);
-        setDigital(MF_BleCS, GPIO_PIN_RESET);
-
-        HAL_SPI_TransmitReceive(&hspi3, master_header, slave_header, 5, 1);
-
-        // let's get the size of data available
-        int dataSize;
-        dataSize = (slave_header[3] | slave_header[4] << 8);
-        int i;
+        // let's fill the get the bytes availables and insert them into the container variable
         char dummy = 0xff;
-
-        if (dataSize > size)
-        {
-            dataSize = size;
-        }
-
-        if (dataSize > 0)
-        {
-            // let's fill the get the bytes availables and insert them into the container variable
-            for (i = 0; i < dataSize; i++)
-            {
-                HAL_SPI_TransmitReceive(&hspi3, (uint8_t *)&dummy, container + i, 1, 1);
-            }
-            setDigital(MF_BleCS, GPIO_PIN_SET);
-        }
-        else
-        {
-            setDigital(MF_BleCS, GPIO_PIN_SET);
-            return -1;
-        }
-
-        // let's stop the SPI2
-
-        return BLE_OK;
+        for (int i = 0; i < dataSize; i++)
+            HAL_SPI_TransmitReceive(&hspi3, (uint8_t *)&dummy, container + i, 1, 1);
+        
+        setDigital(MF_BleCS, GPIO_PIN_SET);
     }
     else
     {
-        return -2;
+        setDigital(MF_BleCS, GPIO_PIN_SET);
+        return BE_ERROR_NO_DATA;
     }
+
+    // let's stop the SPI2
+    return BE_OK;
+        
 }
 
 int checkEventResp(uint8_t *event, uint8_t *reference, int size)
 {
-    int j = 0;
-
-    for (j = 0; j < size; j++)
-    {
-
+    for (int j = 0; j < size; j++)
         if (event[j] != reference[j])
-        {
-            return -1;
-        }
-    }
+            return BE_ERROR2;
 
-    return BLE_OK;
+    return BE_OK;
 }
 
 // TODO make it not blocking function
 void sendCommand(uint8_t *command, int size)
 {
 
-    uint8_t master_header[] = {0x0a, 0x00, 0x00, 0x00, 0x00};
+    uint8_t * master_header = sendMasterHeader;
     uint8_t slave_header[5];
 
     int result;
@@ -241,9 +231,7 @@ void sendCommand(uint8_t *command, int size)
             result = 0;
         }
         else
-        {
             result = -1;
-        }
         // HAL_GPIO_WritePin(CPU_LED_GPIO_Port,CPU_LED_Pin,GPIO_PIN_RESET);
         setDigital(MF_BleCS, GPIO_PIN_SET);
     } while (result != 0);
@@ -252,19 +240,15 @@ void sendCommand(uint8_t *command, int size)
 void catchBLE()
 {
 
-    int result = fetchBleEvent(buffer, 127);
-    if (result == BLE_OK)
+    if (fetchBleEvent(buffer, 127) != BE_OK)
     {
-        if (checkEventResp(buffer, EVENT_DISCONNECTED, 3) == BLE_OK)
-        {
-            setConnectable();
-        }
+        // Big Error 
+        return;
     }
-    else
-    {
-        // HAL_GPIO_TogglePin(CHG_LED2_GPIO_Port,CHG_LED2_Pin);
-        // something bad is happening if I am here
-    }
+    if (checkEventResp(buffer, EVENT_DISCONNECTED, 3) == BE_OK)
+        setConnectable();
+
+    
 }
 
 void setConnectable()
@@ -272,31 +256,30 @@ void setConnectable()
     uint8_t *rxEvent;
     // Start advertising
     uint8_t *localname;
-    int res;
-    localname = (uint8_t *)malloc(sizeof(deviceName) + 5); // carattere di terminazione+listauid+slavetemp
+    size_t deviceNameSize = sizeof(deviceName);
+    size_t terminationSize = 5;
+    localname = (uint8_t *)malloc(deviceNameSize + terminationSize); // carattere di terminazione+listauid+slavetemp
     memcpy(localname, deviceName, sizeof(deviceName));
-    localname[sizeof(deviceName) + 1] = 0x00;
-    localname[sizeof(deviceName) + 2] = 0x00;
-    localname[sizeof(deviceName) + 3] = 0x00;
-    localname[sizeof(deviceName) + 4] = 0x00;
-    localname[sizeof(deviceName)] = 0x00;
+    for(uint8_t i = 0; i < terminationSize; i++)
+        localname[deviceNameSize+i] = 0x00;
 
-    ACI_GAP_SET_DISCOVERABLE[11] = sizeof(deviceName) + 1;
-    ACI_GAP_SET_DISCOVERABLE[3] = sizeof(deviceName) + 5 + sizeof(ACI_GAP_SET_DISCOVERABLE) - 4;
+    ACI_GAP_SET_DISCOVERABLE[11] = deviceNameSize + 1;
+    size_t defineDiscoverableSize = sizeof(ACI_GAP_SET_DISCOVERABLE);
+    ACI_GAP_SET_DISCOVERABLE[3] = deviceNameSize + terminationSize + defineDiscoverableSize - 4;
 
     uint8_t *discoverableCommand;
-    discoverableCommand = (uint8_t *)malloc(sizeof(ACI_GAP_SET_DISCOVERABLE) + sizeof(deviceName) + 5);
-    memcpy(discoverableCommand, ACI_GAP_SET_DISCOVERABLE, sizeof(ACI_GAP_SET_DISCOVERABLE));
-    memcpy(discoverableCommand + sizeof(ACI_GAP_SET_DISCOVERABLE), localname, sizeof(deviceName) + 5);
+    discoverableCommand = (uint8_t *)malloc(defineDiscoverableSize + deviceNameSize + terminationSize);
+    memcpy(discoverableCommand, ACI_GAP_SET_DISCOVERABLE, defineDiscoverableSize);
+    memcpy(discoverableCommand + defineDiscoverableSize, localname, deviceNameSize + terminationSize);
 
-    sendCommand(discoverableCommand, sizeof(deviceName) + 5 + sizeof(ACI_GAP_SET_DISCOVERABLE));
+    sendCommand(discoverableCommand, deviceNameSize + 5 + defineDiscoverableSize);
     rxEvent = (uint8_t *)malloc(7);
 
-    res = fetchBleEvent(rxEvent, 7);
-    if (res == BLE_OK)
+    int res = fetchBleEvent(rxEvent, 7);
+    if (res == BE_OK)
     {
         res = checkEventResp(rxEvent, ACI_GAP_SET_DISCOVERABLE_COMPLETE, 7);
-        if (res == BLE_OK)
+        if (res == BE_OK)
         {
             stackInitCompleteFlag |= 0x80;
         }
@@ -310,13 +293,12 @@ void setConnectable()
 
 int BLE_command(uint8_t *command, int size, uint8_t *result, int sizeRes, int returnHandles)
 {
-    int response;
 
     sendCommand(command, size);
     rxEvent = (uint8_t *)malloc(sizeRes + 2 * returnHandles);
 
     long contatore = 0;
-    while (!readDigital(MF_BleInt))
+    while (!readDigital(MF_BleInt)) // 30000 cicli??
     {
         contatore++;
         if (contatore > 30000)
@@ -325,11 +307,10 @@ int BLE_command(uint8_t *command, int size, uint8_t *result, int sizeRes, int re
         }
     }
 
-    response = fetchBleEvent(rxEvent, sizeRes + returnHandles * 2);
-    if (response == BLE_OK)
-    {
+    int response = fetchBleEvent(rxEvent, sizeRes + returnHandles * 2);
+    if (response == BE_OK)
         response = checkEventResp(rxEvent, result, sizeRes);
-    }
+    
     sleep(10);
 
     return response;
@@ -337,11 +318,10 @@ int BLE_command(uint8_t *command, int size, uint8_t *result, int sizeRes, int re
 
 void addService(uint8_t *UUID, uint8_t *handle, int attributes)
 {
-
     // memcpy
     memcpy(ADD_CUSTOM_SERVICE + 5, UUID, 16);
     ADD_CUSTOM_SERVICE[22] = attributes;
-    if (BLE_command(ADD_CUSTOM_SERVICE, sizeof(ADD_CUSTOM_SERVICE), ADD_CUSTOM_SERVICE_COMPLETE, sizeof(ADD_CUSTOM_SERVICE_COMPLETE), 1) == BLE_OK)
+    if (BLE_command(ADD_CUSTOM_SERVICE, sizeof(ADD_CUSTOM_SERVICE), ADD_CUSTOM_SERVICE_COMPLETE, sizeof(ADD_CUSTOM_SERVICE_COMPLETE), 1) == BE_OK)
     {
         handle[0] = rxEvent[7];
         handle[1] = rxEvent[8];
@@ -358,7 +338,7 @@ void addCharacteristic(uint8_t *UUID, uint8_t *handleChar, uint8_t *handleServic
     ADD_CUSTOM_CHAR[5] = handleService[1];
     ADD_CUSTOM_CHAR[23] = maxsize;
     ADD_CUSTOM_CHAR[25] = proprieties;
-    if (BLE_command(ADD_CUSTOM_CHAR, sizeof(ADD_CUSTOM_CHAR), ADD_CUSTOM_CHAR_COMPLETE, sizeof(ADD_CUSTOM_CHAR_COMPLETE), 1) == BLE_OK)
+    if (BLE_command(ADD_CUSTOM_CHAR, sizeof(ADD_CUSTOM_CHAR), ADD_CUSTOM_CHAR_COMPLETE, sizeof(ADD_CUSTOM_CHAR_COMPLETE), 1) == BE_OK)
     {
         handleChar[0] = rxEvent[7];
         handleChar[1] = rxEvent[8];
