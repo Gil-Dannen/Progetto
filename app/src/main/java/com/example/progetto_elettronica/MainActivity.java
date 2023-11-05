5c169353e0e94ff5120ec407d8280345facdb4ac
Gil-Dannen <davidefalzi70@gmail.com>
2023-11-05 14:31:30 +0100

    aggiunta logica di ricezione

--- app/src/main/java/com/example/progetto_elettronica/MainActivity.java
+++ app/src/main/java/com/example/progetto_elettronica/MainActivity.java
@@ -30,6 +30,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 
+
 import java.util.ArrayList;
 
 import java.util.List;
@@ -46,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
     private int counter=0;
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
-
-
+    public static final int MY_PERMISSION = 100;
+    private Rilevazione rilevazione;
+    private String current;
 
 
     @Override
@@ -58,34 +60,32 @@ public class MainActivity extends AppCompatActivity {
         bluetoothAdapter = manager.getAdapter();
         bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
         handler = new Handler();
+        rilevazione= new Rilevazione();
 
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
-        // Assicurati che il dispositivo supporti BLE
-        if (bluetoothAdapter == null || !getPackageManager().hasSystemFeature("android.hardware.bluetooth_le")) {
-            // Il dispositivo non supporta BLE
-            // Agisci di conseguenza
-        }
+
     }
 
 
     private final ScanCallback leScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
-            Log.i("risultati",result.getDevice().toString());
+           // Log.i("risultati",result.getDevice().toString());
             if (!isConnected && result.getDevice().toString().equals("C8:18:76:8B:67:0B")) {
-                Log.i("risultati","procede");
-                connectToBLEDevice("C8:18:76:8B:67:0B");
-            }else if(isConnected){
-                Log.i("Conferma","Scansione terminata");
-            }else
-               Log.i("Valore","nessun interesse");
+                //Log.i("risultati","procede");
+                connectToBLEDevice();
+            }/*else if(isConnected){
+                //Log.i("Conferma","Scansione terminata");
+            }else;
+               //Log.i("Valore","nessun interesse");
+               */
 
         }
     };
 
     public void scanLeDevice(View v) {
-        if (!scanning) {
+        if (!scanning || !isConnected) {
             // Stops scanning after a predefined scan period.
             handler.postDelayed(new Runnable() {
                 @Override
@@ -117,26 +117,6 @@ public class MainActivity extends AppCompatActivity {
             bluetoothLeScanner.stopScan(leScanCallback);
         }
     }
-
-    /*
-        public void scanLeDevice(View v) {
-            if (!scanning) {
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        scanning = false;
-                        stopScanning();
-                    }
-                }, SCAN_PERIOD);
-
-                scanning = true;
-                startScanning();
-            } else {
-                scanning = false;
-                stopScanning();
-            }
-        }
-    */
     private void startScanning() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= 31) {
@@ -153,19 +133,21 @@ public class MainActivity extends AppCompatActivity {
             Log.e("Errore :","Posizione non gradita");
         }
         bluetoothLeScanner.stopScan(leScanCallback);
-        Log.i("Successo :","Scansione arestata con successo");
+        //Log.i("Successo :","Scansione arestata con successo");
     }
 
 
 
 
     // Metodo per connettersi a un dispositivo BLE specifico
-    private void connectToBLEDevice(String deviceAddress) {
-        Log.i("Inizio Connessione","dispositivo "+ deviceAddress);
-        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+    private void connectToBLEDevice() {
+        //Log.i("Inizio Connessione","dispositivo "+ deviceAddress);
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice("C8:18:76:8B:67:0B");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            Log.i("Esito","errato, niente permessi");
-            return;
+           // Log.i("Esito","errato, niente permessi");
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.BLUETOOTH_CONNECT
+            },MY_PERMISSION);
         }
         if (bluetoothGatt != null) {
             bluetoothGatt.close();
@@ -179,19 +161,19 @@ public class MainActivity extends AppCompatActivity {
     private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            Log.i("Connessione In corso","Fin qui tutto nella norma");
+           // Log.i("Connessione In corso","Fin qui tutto nella norma");
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-                Log.i("Connesso", "Connessione avvenuta con successo");
+              //  Log.i("Connesso", "Connessione avvenuta con successo");
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
                     isConnected = true;
                     if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
-
+                        Log.e("Errore", "Connessione non avvenuta con successo");
                     }
-                    bluetoothLeScanner.stopScan(leScanCallback);
-                    //gatt.discoverServices();
+                    stopScanning();
+                    gatt.discoverServices();
                 }
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                Log.i("Errore", "Connessione non avvenuta con successo");
+               //Log.i("Errore", "Connessione non avvenuta con successo");
                 gatt.close();
                 isConnected = false;
                 startScanning();
@@ -206,17 +188,81 @@ public class MainActivity extends AppCompatActivity {
 
 
         @Override
+        public void onCharacteristicRead (BluetoothGatt gatt, BluetoothGattCharacteristic characteristic,int status) {
+            //byte[] data = characteristic.getValue();
+            String dataString = characteristic.getStringValue(0);
+           // Log.i("BLE", "Dati letti: " + dataString);
+
+            try {
+
+            if (dataString.contains("Temperature")) {
+                rilevazione.setTemperatura(dataString.replaceAll("\\s", "").split(":")[1]);
+                //Log.i("test",dataString.replaceAll("\\s","").split(":")[1]);
+            } else if (dataString.contains("Humidity")) {
+                rilevazione.setUmidita(dataString.replaceAll("\\s", "").split(":")[1]);
+            } else if (dataString.contains("Pressure")) {
+                rilevazione.setPressione(dataString.replaceAll("\\s", "").split(":")[1]);
+            } else if (dataString.contains("Inertia") || dataString.contains("magnetic") || dataString.contains("gyroscope")) {
+                current=dataString.replaceAll("\\s","").split(":")[1];
+                //Log.i("test",dataString.replaceAll("\\s","").split(":")[1]);
+            }
+
+               if (dataString.contains("X")) {
+                   switch (current) {
+                       case "Inertia":
+                           rilevazione.setInertiaX(dataString.replaceAll("\\s", "").split(":")[1]);
+
+                       case "magnetic":
+                           rilevazione.setMagneX(dataString.replaceAll("\\s", "").split(":")[1]);
+                       case "gyroscope":
+                           rilevazione.setGyroX(dataString.replaceAll("\\s", "").split(":")[1]);
+                   }
+               } else if (dataString.contains("Y")) {
+                   switch (current) {
+                       case "Inertia":
+                           rilevazione.setInertiaY(dataString.replaceAll("\\s", "").split(":")[1]);
+
+                       case "magnetic":
+                           rilevazione.setMagneY(dataString.replaceAll("\\s", "").split(":")[1]);
+                       case "gyroscope":
+                           rilevazione.setGyroY(dataString.replaceAll("\\s", "").split(":")[1]);
+                   }
+
+               } else if (dataString.contains("Z"))
+                   switch (current) {
+                       case "Inertia":
+                           rilevazione.setInertiaZ(dataString.replaceAll("\\s", "").split(":")[1]);
+
+                       case "magnetic":
+                           rilevazione.setMagneZ(dataString.replaceAll("\\s", "").split(":")[1]);
+                       case "gyroscope":
+                           rilevazione.setGyroZ(dataString.replaceAll("\\s", "").split(":")[1]);
+                   }
+           }catch(Exception ex){
+               Log.e("Errore",ex.toString());
+           }
+        }
+
+        @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-            Log.i("Servizii :","trovati");
+           // Log.i("Servizi :","trovati");
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 List<BluetoothGattService> services = gatt.getServices();
                 List<BluetoothGattCharacteristic> allCharacteristics = new ArrayList<>();
-                Log.i("Servizi :","Raccolti");
+               // Log.i("Servizi :","Raccolti");
+                //int conta =0;
                 for (BluetoothGattService service : services) {
                     List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
+/*
+                    for(BluetoothGattCharacteristic charateristic : characteristics){
+                        Log.i("Caratteristica ", charateristic.getValue()+" "+conta++);
+
+                    }
+
+ */
                     allCharacteristics.addAll(characteristics);
-                    Log.i("Caratteristiche :","Raccolte");
                 }
+               // Log.i("Caratteristiche :","Raccolte");
 
                 // Inizia il ciclo di lettura continua delle caratteristiche
                 readCharacteristicsInLoop(gatt, allCharacteristics);
@@ -224,6 +270,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Errore", "Scoperta dei servizi fallita");
             }
         }
+
 
         // Funzione per leggere continuamente le caratteristiche in un ciclo
         private void readCharacteristicsInLoop(final BluetoothGatt gatt, final List<BluetoothGattCharacteristic> characteristics) {
@@ -233,16 +280,25 @@ public class MainActivity extends AppCompatActivity {
             final Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                    Log.i("Lettura.Caratteristiche :","In corso");
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
+                        return;
+
+                    //Log.i("Lettura.Caratteristiche :","In corso");
                     if (counter < characteristics.size()) {
-                        BluetoothGattCharacteristic characteristic = characteristics.get(counter);
-                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+
+                        try {
+                            BluetoothGattCharacteristic characteristic = characteristics.get(counter);
                             gatt.readCharacteristic(characteristic);
-                            counter=0;
+
+                            counter++;
+                        }catch(Exception errore){
+                            Log.e("Catch",errore.toString());
                         }
                     } else {
                         // Se hai letto tutte le caratteristiche, ripeti il ciclo
+                        //Log.i("Stringa",rilevazione.toString());
                         counter=0;
+                        rilevazione.dump();
                     }
                     // Richiama il runnable con il ritardo specificato
                     handler.postDelayed(this, delay);
@@ -255,4 +311,22 @@ public class MainActivity extends AppCompatActivity {
 
 
     };
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSION: {
+                // Se la richiesta è stata cancellata, l'array result è vuoto
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // utilizza permesso
+                } else {
+                    // gestisci permesso negato
+                }
+
+            }
+            // altri casi
+        }
+    }
 }
---
+++ app/src/main/java/com/example/progetto_elettronica/Rilevazione.java
@@ -0,0 +1,128 @@
+package com.example.progetto_elettronica;
+
+public class Rilevazione {
+    private String temperatura;
+    private String umidita;
+    private String pressione;
+    private String inertiaX;
+    private String inertiaY;
+    private String inertiaZ;
+    private String gyroX;
+    private String gyroY;
+    private String gyroZ;
+    private String magneX;
+    private String magneY;
+    private String magneZ;
+
+
+
+    public Rilevazione() {
+        
+    }
+
+    public String getTemperatura() {
+        return temperatura;
+    }
+
+    public String getUmidita() {
+        return umidita;
+    }
+
+    public String getPressione() {
+        return pressione;
+    }
+
+    public String getInertia() {
+        return inertiaX;
+    }
+
+
+    public String getGyro() {
+        return gyroX;
+    }
+
+    public String getMagne() {
+        return magneX;
+    }
+
+    public void setTemperatura(String temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public void setUmidita(String umidita) {
+        this.umidita = umidita;
+    }
+
+    public void setPressione(String pressione) {
+        this.pressione = pressione;
+    }
+
+    public void setInertiaX(String inertiaX) {
+        this.inertiaX = inertiaX;
+    }
+
+    public void setInertiaY(String inertiaY) {
+        this.inertiaY = inertiaY;
+    }
+
+    public void setInertiaZ(String inertiaZ) {
+        this.inertiaZ = inertiaZ;
+    }
+
+    public void setGyroX(String gyroX) {
+        this.gyroX = gyroX;
+    }
+
+    public void setGyroY(String gyroY) {
+        this.gyroY = gyroY;
+    }
+
+    public void setGyroZ(String gyroZ) {
+        this.gyroZ = gyroZ;
+    }
+
+    public void setMagneX(String magneX) {
+        this.magneX = magneX;
+    }
+
+    public void setMagneY(String magneY) {
+        this.magneY = magneY;
+    }
+
+    public void setMagneZ(String magneZ) {
+        this.magneZ = magneZ;
+    }
+
+    public void dump(){
+        this.temperatura = "";
+        this.umidita = "";
+        this.pressione = "";
+        this.inertiaX = "";
+        this.inertiaY = "";
+        this.inertiaZ = "";
+        this.gyroX = "";
+        this.gyroY = "";
+        this.gyroZ = "";
+        this.magneX = "";
+        this.magneY = "";
+        this.magneZ = "";
+    }
+
+    @Override
+    public String toString() {
+        return "Rilevazione{" +
+                "temperatura=" + temperatura +
+                ", umidita=" + umidita +
+                ", pressione=" + pressione +
+                ", inertiaX=" + inertiaX +
+                ", inertiaY=" + inertiaY +
+                ", inertiaZ=" + inertiaZ +
+                ", gyroX=" + gyroX +
+                ", gyroY=" + gyroY +
+                ", gyroZ=" + gyroZ +
+                ", magneX=" + magneX +
+                ", magneY=" + magneY +
+                ", magneZ=" + magneZ +
+                '}';
+    }
+}
